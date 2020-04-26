0052f03a6dd0644cea27fb92e6a6bb4560b85b32
lx70770 <lx70770@MacBook-Pro.local>
2020-04-26 09:59:16 +0800

    :sparkles: core

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,6 +1,6 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { parseHeaders } from './helpers/headers'
-import { createError } from './helpers/error'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import { parseHeaders } from '../helpers/headers'
+import { createError } from '../helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -17,7 +17,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     }
 
     // 初始化请求
-    request.open(method.toUpperCase(), url, true)
+    request.open(method.toUpperCase(), url!, true)
 
     // 请求成功后处理逻辑
     request.onreadystatechange = function handleLoad() {
